7c12d23c2bb2e64ecdc31bb87ec8ca1507c79599
Alexander Hengsteler <a.hengsteler@gmx.de>
2020-05-09 10:26:01 +0200

    update generator main

--- greenlake_data_generator/src/main/java/Main.java
+++ greenlake_data_generator/src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("Willkommen beim Smart Greenhouse Daten Generator.\n" +
                 "Diese Anwendung generiert automatisch Daten, die von einem Smart Greenhouse stammen könnten und bieten dadurch die " +
                 "Möglichkeit ein solches zu simulieren.");
-        System.out.println("Geben Sie zunächst ein, welche der folgenden Varianten genutzt werden soll:\n" +
+        System.out.println("Geben Sie ein, welche der folgenden Varianten genutzt werden soll:\n" +
                 "\t1. Standard Prototyp (4x moisture, 2x temp, 2x humidity, 1x brightness)\n" +
                 "\t2. Alternative 1 (2x moisture, 2x temp, 2x humidity, 1x brightness)");
         boolean valid = false;
@@ -29,26 +29,14 @@ public class Main {
                 System.out.println("Ungültige Eingabe! Bitte geben Sie eine gültige Nummer ein.");
             }
         }
-        System.out.print("Geben Sie eine ID für das Greenhouse an. Sollten mehrere Greenhouses simuliert werden, bitte mit anderen Quellen abstimmen, damit jedes eine andere ID verwendet: ");
-        valid = false;
-        int id = 0;
-        while(valid == false) {
-            String idString = readConsole();
-            try {
-                id = Integer.parseInt(idString);
-                valid = true;
-            }
-            catch (Exception e) {
-                System.out.println("Ungültige Eingabe! Bitte geben Sie eine gültige ID ein.");
-            }
-        }
 
-        System.out.println("Beginne mit der Generierung von Daten für das Greenhouse mit der Id " + id);
-        GeneratorThread thread = new GeneratorThread(id, alternative);
+        System.out.println("Beginne mit der Generierung von Daten für das Greenhouse mit der Id " + alternative);
+        GeneratorThread thread = new GeneratorThread(alternative, alternative);
         thread.run();
         logger.info("Generator thread started");
-        System.out.println("Drücke eine beliebige Taste, um die Anwendung zu schließen.");
+        System.out.println("Drücke Enter, um die Anwendung zu schließen.");
         readConsole();
+        System.out.println("Beende Applikation");
         thread.stopExecution();
     }
 
